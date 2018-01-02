a9169af1697fa32d1359b46712b49fb5444d85fc
Daisuke AIBA <aiba@dreamgarage.jp>
2018-01-02 17:46:33 +0900

    base プロジェクトを元に、Spring Data JPA のサンプルプロジェクトのディレクトリを作成

---
+++ spring-data-jpa/src/main/java/jp/aibax/App.java
@@ -0,0 +1,14 @@
+package jp.aibax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App
+{
+    public static void main(String[] args)
+    {
+        /* アプリケーションを起動 */
+        SpringApplication.run(App.class, args);
+    }
+}
