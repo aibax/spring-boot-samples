9198a9ed0d6ea8e19ce0da0b71b7de0a695f3813
Daisuke AIBA <aiba@dreamgarage.jp>
2018-01-04 02:46:55 +0900

    Spring Security 標準のユーザーを管理するクラスを拡張し、ログインIDに使用するユーザー名とは別の表示用の名前を使用するサンプルを追加

--- spring-security/src/main/java/jp/aibax/configuration/SecurityConfig.java
+++ spring-security/src/main/java/jp/aibax/configuration/SecurityConfig.java
@@ -7,13 +7,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import jp.aibax.security.User;
 import jp.aibax.service.SampleUserStore;
 
 @Configuration
--- spring-security/src/main/java/jp/aibax/service/SampleUserStore.java
+++ spring-security/src/main/java/jp/aibax/service/SampleUserStore.java
@@ -8,9 +8,10 @@ import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import jp.aibax.security.User;
 
 @Service
 public class SampleUserStore
@@ -18,21 +19,21 @@ public class SampleUserStore
     private static Map<String, User> USERS = new HashMap<String, User>()
     {
         {
-            put("admin", createUser("admin", "secret", "ROLE_USER", "ROLE_ADMINISTRATOR"));
-            put("manager", createUser("manager", "secret", "ROLE_USER"));
-            put("developer", createUser("developer", "secret", "ROLE_USER"));
-            put("guest", createUser("guest", "secret", "ROLE_USER"));
+            put("admin", createUser("admin", "secret", "Administrator", "ROLE_USER", "ROLE_ADMINISTRATOR"));
+            put("manager", createUser("manager", "secret", "Manager", "ROLE_USER"));
+            put("developer", createUser("developer", "secret", "Developer", "ROLE_USER"));
+            put("guest", createUser("guest", "secret", "Guest", "ROLE_USER"));
         }
     };
 
-    private static User createUser(String username, String rawPassword, String... roles)
+    private static User createUser(String username, String rawPassword, String displayName, String... roles)
     {
         String password = new BCryptPasswordEncoder().encode(rawPassword);
 
         Set<GrantedAuthority> authorities = new HashSet<>();
         Arrays.asList(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
 
-        return new User(username, password, authorities);
+        return new User(username, password, displayName, authorities);
     }
 
     public User findByLoginId(String loginId)
@@ -50,6 +51,6 @@ public class SampleUserStore
         }
 
         // Spring Security のログアウト処理でパスワードが初期化されるためコピーを返している
-        return new User(user.getUsername(), user.getPassword(), user.getAuthorities());
+        return new User(user.getUsername(), user.getPassword(), user.getDisplayName(), user.getAuthorities());
     }
 }
