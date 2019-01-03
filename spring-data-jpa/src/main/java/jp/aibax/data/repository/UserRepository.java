5c8748b7582f6f1c40621fdb65e2ed91cf2607f6
Daisuke AIBA <aiba@dreamgarage.jp>
2019-01-03 22:06:37 +0900

    Spring Boot のバージョンを2.1系に更新

--- spring-data-jpa/src/main/java/jp/aibax/data/repository/UserRepository.java
+++ spring-data-jpa/src/main/java/jp/aibax/data/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package jp.aibax.data.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jp.aibax.data.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>
 {
-    User findByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
 }
